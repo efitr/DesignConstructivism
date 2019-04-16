5702b23c8eb72f23f7868d4974bcbf31eb86e612
Egon Fiedler <efiedler93@gmail.com>
2019-04-16 14:34:32 -0700

    Empty Project, First iteration of Readme

---
+++ DesignConstructivism/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignConstructivism
+//
+//  Created by Egon Fiedler on 4/16/19.
+//  Copyright © 2019 App Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Project Characteristics
+- The project name is “Design Constructivism”
+- MVVM Architecture
+- Agile Development Project Approach
+- Test Driven Development Software Design Approach
+- Swift 5 Programming Language
+- Code Conventions Apple
+- Made by Egon Fiedler
